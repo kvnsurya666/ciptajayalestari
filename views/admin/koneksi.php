56e352b980c329f0d53ca21971e405f88c5f8c76
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-21 13:29:09 +0700

    koneksi db

--- views/admin/koneksi.php
+++ views/admin/koneksi.php
@@ -2,7 +2,7 @@
 $koneksi = "localhost";
 $username = "root";
 $password = "";
-$database = "ciptajayalestari";
+$database = "mediasol_ciptajayalestari";
 
 // Create connection
 $conn = new mysqli($koneksi, $username, $password, $database);
