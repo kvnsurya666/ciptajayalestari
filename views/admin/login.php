1d69255161ac184a32b54e5ab84afc72ed513485
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-21 13:49:01 +0700

    test login page

--- views/admin/login.php
+++ views/admin/login.php
@@ -17,7 +17,10 @@ include "koneksi.php";
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.css" rel="stylesheet">
+  <link rel="stylesheet" href="/css/bootstrap.css">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/sb-admin-2.css" >
+  <link rel="stylesheet" href="/css/sb-admin-2.min.css">
 
 </head>
 
