004678a6bdb0da920ca37c2c40c44c008f2bca17
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-21 12:40:46 +0700

    masukan admin

---
+++ views/admin/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$koneksi = "localhost";
+$username = "root";
+$password = "";
+$database = "projectta";
+
+// Create connection
+$conn = new mysqli($koneksi, $username, $password, $database);
+
+?>
