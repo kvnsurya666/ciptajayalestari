4bc96e68a2816c1c7b204789b3b517fe4478c040
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-21 13:52:17 +0700

    ganti gambar login

--- views/admin/login.php
+++ views/admin/login.php
@@ -37,7 +37,9 @@ include "koneksi.php";
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block">
+                <img src="/assets/img/logo ciptajayalestari.PNG" alt="Logo Cipta Jaya Lestari">
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
