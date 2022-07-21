72dc0ae99bde85675b92e3d55b67edc9036cc06f
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-22 00:10:04 +0700

    test tentang kami

---
+++ views/about/tentangkami.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>CIPTA JAYA LESTARI</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link rel="stylesheet" href="/css/styles.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/bootstrap.css" />
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/assets/font-awesome/font-awesome.css" />
+    <link rel="stylesheet" href="/assets/font-awesome/font-awesome.min.css" />
+  </head>
+  <body>
+    <!-- Navigation-->
+
+    <nav class="navbar-dark sticky-top bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-1">
+            <a class="navbar-brand" href="#" style="padding-top: 10px; padding-bottom: 10px">
+              <img src="assets/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
+            </a>
+          </div>
+
+          <div class="col-md-11" id="navbarNav">
+            <ul class="nav justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/views/katalog/produk.php" role="button" aria-expanded="false">Katalog</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/views/katalog/mesin.php">Mesin</a></li>
+                  <li><a class="dropdown-item" href="/views/katalog/oli.php">Oli</a></li>
+                  <li><a class="dropdown-item" href="/views/katalog/aksesoris.php">Aksesoris</a></li>
+                  <li><a class="dropdown-item" href="/views/katalog/jasa.php">Jasa</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/views/about/tentangkami.php">Tentang Kami</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Masthead-->
+    <div class="container-fluid">
+      <div id="carouselSlide" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/assets/img/slider1.png" class="d-block w-100" alt="Gambar 1" />
+          </div>
+          <div class="carousel-item">
+            <img src="/assets/img/slider2.png" class="d-block w-100" alt="Gambar 2" />
+          </div>
+          <div class="carousel-item">
+            <img src="/assets/img/slider3.png" class="d-block w-100" alt="Gambar 3" />
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselSlide" data-bs-slide="prev">
+          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselSlide" data-bs-slide="next">
+          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
+    </div>
+    <!-- Company Profile -->
+    <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Profil Perusahaan</h2>
+                        <p class="lead">CV. CIPTA JAYA LESTARI merupakan Agen Resmi penjualan Mesin Mesin Merk Vertex.</p>
+                        <p class="lead">Selain itu kami juga sebagai Distributor / Supplier berbagai mesin mesin untuk memenuhi kebutuhan industri Anda, seperti :</p>
+                        <ul>
+                            <li>Mesin CNC Milling</li>
+                            <li>Mesin Bubut</li>
+                            <li>Mesin Bubut CNC</li>
+                            <li>VERTEX TOOLS</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- Our Team Showcase -->
+   <section class="testimonials text-center bg-light">
+            <div class="container">
+                <h2 class="mb-5">What people are saying...</h2>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-1.jpg" alt="..." />
+                            <h5>Margaret E.</h5>
+                            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-2.jpg" alt="..." />
+                            <h5>Fred S.</h5>
+                            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-3.jpg" alt="..." />
+                            <h5>Sarah W.</h5>
+                            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    
+
+    <!-- Footer-->
+    <div class="footer_agileinfo_w3">
+      <div class="footer_inner_info_w3ls_agileits">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>
+              <a href="/">
+                <img src="/assets/img/logo ciptajayalestari.PNG" style="margin-top: -10px" width="100px" />
+              </a>
+            </h2>
+            <h4><b>Metal working and machinery</b></h4>
+          </div>
+          <div class="col-md-4 footer-center">
+            <div class="sign-grds">
+              <div class="col-md-5 sign-gd-one">
+                <h4>Hubungi <span>Kami</span></h4>
+                <div class="address">
+                  <div class="address-grid">
+                    <div class="address-right">
+                      <h6>Layanan Pelanggan</h6>
+                      <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                      <div class="phone-domain">
+                        <a href="tel:0313896920" class="nobor">0313896920</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="address-grid">
+                  <div class="address-right">
+                    <h6>E-mail</h6>
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <p><a href="mailto:ciptajayalestari@gmail.com" style="color: white">ciptajayalestari@gmail.com</a></p>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- WhatsApp -->
+          <div class="col-md-4">             
+          <div class="sign-gd-two emailprofile cta-footer text-center footer-right" id="wa_company" data-wa="6281335700084;6281330066919;628123170863;6281254693810;6281331637497;085232777499" data-company="//www.ciptajayalestari.com" data-member="2">
+            <div id="wa">
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Sales Counter</b></p>
+                  <img src="/assets/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281335700084&amp;text=Halo Sales Counter, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081335700084</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Edi</b></p>
+                  <img src="/assets/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=628123170863&amp;text=Halo Pak Edi, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">08123170863</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Ilyas</b></p>
+                  <img src="/assets/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281254693810&amp;text=Halo Pak Ilyas, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081254693810</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Rochman</b></p>
+                  <img src="/assets/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281331637497&amp;text=Halo Pak Rochman, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081331637497</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Dwiono</b></p>
+                  <img src="/assets/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6285232777499&amp;text=Halo Pak Dwiono, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">085232777499</span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap-3.1.1.min.js"></script>
+    <script type="text/javascript" src="/js/move-top.js"></script>
+    <script type="text/javascript" src="/js/imagzoom.js"></script>
+    <script type="text/javascript" src="/js/jquery.flexslider.js"></script>
+
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <script>
+      // Can also be used with $(document).ready()
+      $(document).ready(function () {
+        $(".flexslider").flexslider({
+          animation: "slide",
+          controlNav: "thumbnails",
+        });
+      });
+    </script>
+  </body>
+</html>
