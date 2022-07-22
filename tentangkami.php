6ea3aaf322e8085c53511d0294d7f003cce567f0
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-22 15:11:28 +0700

    big changes

--- tentangkami.php
+++ tentangkami.php
@@ -27,7 +27,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-1">
-            <a class="navbar-brand" href="/views/index.html" style="padding-top: 10px; padding-bottom: 10px">
+            <a class="navbar-brand" href="index.html" style="padding-top: 10px; padding-bottom: 10px">
               <img src="/assets/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
             </a>
           </div>
@@ -35,19 +35,19 @@
           <div class="col-md-11" id="navbarNav">
             <ul class="nav justify-content-center">
               <li class="nav-item">
-                <a class="nav-link active" href="/views/index.html">Home</a>
+                <a class="nav-link active" href="index.html">Home</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/views/katalog/produk.php" role="button" aria-expanded="false">Katalog</a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="/views/katalog/mesin.php">Mesin</a></li>
-                  <li><a class="dropdown-item" href="/views/katalog/oli.php">Oli</a></li>
-                  <li><a class="dropdown-item" href="/views/katalog/aksesoris.php">Aksesoris</a></li>
-                  <li><a class="dropdown-item" href="/views/katalog/jasa.php">Jasa</a></li>
+                  <li><a class="dropdown-item" href="mesin.php">Mesin</a></li>
+                  <li><a class="dropdown-item" href="oli.php">Oli</a></li>
+                  <li><a class="dropdown-item" href="aksesoris.php">Aksesoris</a></li>
+                  <li><a class="dropdown-item" href="jasa.php">Jasa</a></li>
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/views/about/tentangkami.php">Tentang Kami</a>
+                <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
               </li>
             </ul>
           </div>
