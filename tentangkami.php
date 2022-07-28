6f09f7411324019bdee073245f1b941e9f7ae81e
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-28 19:08:55 +0700

    index php

--- tentangkami.php
+++ tentangkami.php
@@ -37,7 +37,7 @@
           <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" href="index.html">Home</a>
+                <a class="nav-link active" href="index.php">Home</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/views/katalog/produk.php" role="button" aria-expanded="false">Catalogue</a>
