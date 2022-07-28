d4c910dd5c09eb642b4514c6aa94346b5a7c4df7
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-28 12:12:01 +0700

    nambah css baru

--- mesin.php
+++ mesin.php
@@ -56,6 +56,7 @@
       </div>
     </nav>
 
+    <!-- Banner -->
     <div class="container-fluid">
         <div class="row">
             <div class="col">
@@ -63,10 +64,18 @@
                 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Katalog</li>
+                        <li class="breadcrumb-item active" aria-current="page">Catalogue</li>
                         <li class="breadcrumb-item active" aria-current="page">Machinery</li>
                     </ol>
                 </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                
             </div>
         </div>
     </div>
